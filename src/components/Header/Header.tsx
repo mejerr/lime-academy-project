260e890e7196e05ab9714f442768b155113b10ce
mejerr <georgy_90@abv.bg~>
2022-03-15 22:55:32 +0200

    Into previous commit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import { RouteComponentProps } from 'react-router';
 import { withRouter } from 'react-router-dom';
 import limeblockLogo from "../../assets/limeblock-logo.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignIn } from '@fortawesome/free-solid-svg-icons';
 import Button from '../Button';
 import Menu from './Menu';
 
@@ -31,8 +33,26 @@ const Header: FC<RouteComponentProps> = ({ history} ) => {
           alignItems={"center"}
           justifyContent={"center"}
         />
+          <Button
+            title={'My Collection'}
+            isActive ={pathname === "/collection"}
+            width={"100px"}
+            onClick={() => push('/collection')}
+            alignItems={"center"}
+            justifyContent={"center"}
+        />
       </NavBar>
-      <SiteTitle>{"Limeblock"}</SiteTitle>
+      <SiteTitle>{"Limeblock Marketplace"}</SiteTitle>
+      <ButtonWrapper>
+        <LoginIcon icon={faSignIn}/>
+        <Button
+          title={'Login'}
+          width={"60px"}
+          onClick={() => push('/collection')}
+          alignItems={"center"}
+          justifyContent={"center"}
+        />
+      </ButtonWrapper>
     </HeaderWrapper>
   )
 }
@@ -43,7 +63,7 @@ const HeaderWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 10px 10px;
+  padding: 10px 0 10px 10px;
   box-shadow: rgb(4 17 29 / 25%) 0px 0px 8px 0px;
 `;
 
@@ -53,6 +73,7 @@ const Logo = styled.div`
   margin-right: 10px;
   background: transparent url(${limeblockLogo}) top center no-repeat;
   background-size: contain;
+  flex-shrink: 0;
 `;
 
 const NavBar = styled.div`
@@ -69,7 +90,25 @@ const SiteTitle = styled.div`
   justify-content: center;
   width: 100px;
   color: black;
-  font-size: 25px;
+  font-size: 40px;
 `;
+
+const ButtonWrapper = styled.div`
+  background-color: #024bb0;
+  display: flex;
+  align-items: center;
+
+  > div {
+    color: white;
+  }
+`;
+
+const LoginIcon = styled(FontAwesomeIcon)`
+  width: 25px;
+  height: 25px;
+  color: white;
+  margin-left: 10px;
+`;
+
 
 export default withRouter(Header);
